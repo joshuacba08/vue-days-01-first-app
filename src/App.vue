5d63e23e9ea4275b7611c016b1019b3188b73fdb
joroya.hms <47667368+joshuacba08@users.noreply.github.com>
2022-08-31 20:07:11 -0300

    clase 31/08/2022

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <h1>Hello World</h1>
-  <Count />
-
+  <div class="app-container">
+    <Saludo />
+  </div>
 </template>
 
 <script>
+  //importaciones
 import Count from './components/Count.vue';
+import Saludo from './components/Saludo.vue';
 
+//exportaciones o declaraciones
 export default {
   name: 'App',
   components: {
-    Count
-  }
+    Count,
+    Saludo
+
+}
 }
 </script>
 
 <style>
-
+  .app-container{
+    min-height: 100vh;
+    background-color: rgb(20, 0, 38);
+    color:aliceblue;
+  }
 </style>
---
+++ src/components/Saludo.vue
@@ -0,0 +1,45 @@
+<template>
+  <p v-if="isMorning" class='saludo-paragraph'>Buen día</p>
+  <p v-if="isAfternoon" class='saludo-paragraph'>Buenas tardes</p>
+  <p v-if="isEvening" class='saludo-paragraph' v-text="saludoNoche"></p>
+  <p v-show="isMorning" class='saludo-paragraph'>Buen día</p>
+
+  <div v-html="html"></div>
+
+  <ul>
+    <li v-for="(fruta,index) in frutas" v-bind:key="index">{{fruta}}</li>
+  </ul>
+  <input type="text" v-bind:placeholder="placeHolder">
+</template>
+
+<script>
+
+
+const date = new Date().getHours();
+console.log(date);
+
+
+export default {
+    name:'Saludo',
+    data: ()=>{
+        return {
+            isMorning: date < 12 && date >= 6,
+            isAfternoon: date>=12 && date < 18,
+            isEvening: date >= 18 || (date >= 0 && date < 6),
+            saludoNoche: 'Buenas noches',
+            html:'<p class="saludo-paragraph"> Este es un párrafo ingresado por v-html </p>',
+            frutas:['🍌','🍍','🍓','🍑','🍐','🍏','🍈','🍎'],
+            placeHolder:'Mi vieja mula ya no es lo que era'
+        }
+    }
+}
+</script>
+
+<style>
+    .saludo-paragraph{
+        text-align: center;
+        margin: 0px auto;
+        padding: 10px 15px;
+        border: 1px solid whitesmoke;
+    }
+</style>
